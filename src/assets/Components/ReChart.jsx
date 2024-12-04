66581b496d141ad7a9869665ed559ecac37472f1
shamim150 <smshamimmahmud150@gmail.com>
2024-12-04 14:27:10 +0600

    add

--- src/assets/Components/ReChart.jsx
+++ src/assets/Components/ReChart.jsx
@@ -64,10 +64,7 @@ const Chart = ()=> {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
           <Tooltip />
-          <Legend />
           <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
           <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
         </BarChart>
